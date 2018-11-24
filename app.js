1193d2a26d72de4809afb5ac06eef3ab2ecc1eae
Keoni Murray <keonimurray45@gmail.com>
2018-11-24 04:58:51 -0800

    cannabis search

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Dependencies
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongojs = require('mongojs');
 
 //intialize express
@@ -8,7 +9,29 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+
+app.use(bodyParser.json());
+
+// app.use(bodyParser.json({
+//     type: ''
+// }));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
 app.use(express.static(__dirname + '/public'));
+
+const databaseUrl = "mongodb://localhost/cannabissearch";
+const collections = ['strains2'];
+
+let db = mongojs(databaseUrl, collections);
+
+db.on('error', (err) => {
+    console.log("Database Error:", error);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -16,6 +39,19 @@ app.get('/', (req, res) => {
 
 app.get('/strain/:name', (req, res) => {
     //this route will query database based on user result and return a json object back
-})
+
+    db.strains2.find({
+        name: req.params.weedstrain,
+    }, (err, found) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(found)
+            res.json(found);
+        }
+    });
+});
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`));
